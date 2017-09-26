e6a4354c9f3ad2ba9f97688bca689d9ea8fd3ab1
evgalp <evgeniyalpayev@gmail.com>
2017-09-26 16:10:08 +0300

    init db script whithin flask added

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,8 +1,8 @@
+-- This schema consists of a single table called entries. Each row in this table has an id, a title, and a text. The id is an automatically incrementing integer and a primary key, the other two are strings that must not be null.
+
 drop table if exists enteries;
 create table enteries (
   id integer primary key autoincrement,
   title text not null,
   'text' text not null
 );
-
--- This schema consists of a single table called entries. Each row in this table has an id, a title, and a text. The id is an automatically incrementing integer and a primary key, the other two are strings that must not be null.
